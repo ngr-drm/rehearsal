084169cffdfed522e994fddbfa7b9f3c09a1a703
nrg-drm <su.re44e@gmail.com>
2024-08-22 13:55:52 -0300

    feat(domain): add entities

---
+++ src/main/java/com/rehearsal/api/domain/address/Address.java
@@ -0,0 +1,21 @@
+package com.rehearsal.api.domain.address;
+
+import com.rehearsal.api.domain.event.Event;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @OneToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
---
+++ src/main/java/com/rehearsal/api/domain/coupon/Coupon.java
@@ -0,0 +1,31 @@
+package com.rehearsal.api.domain.coupon;
+
+import com.rehearsal.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
