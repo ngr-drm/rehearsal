bb25776204ae413ca88ad9a7b98dd06eaaaa413e
nrg-drm <su.re44e@gmail.com>
2024-08-22 13:55:14 -0300

    feat: add code base

---
+++ src/main/java/com/rehearsal/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.rehearsal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
